de5660738ad3857a326a3031021004916cd02192
Yann Letourneur <yann.letourneur@etu.univ-grenoble-alpes.fr>
2025-04-30 21:47:38 +0700

    New separation between public and private constants

---
+++ constants.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+#************************************
+# Author : Yann Letourneur
+# Date : 30 Avril 2025
+# Description : Constants for the project
+# Usage : In another python file :
+#      from constants import ...
+#************************************
+
+from private_constants import dataset_location
+
+# Path
+PATH = dataset_location + "/Colpo1/Colpo Dataset"
+PATH2D = PATH + "/2D"
+PATH3D = PATH + "/3D"
+
+# Status of the patients
+STATUS = ("Cancer", "HSIL", "LSIL", "Normal")
+
+# Contrast mediators = names of the 3 images for each patient
+CONTRAST_MEDIATORS = ("acetic_acid", "lugol", "saline")
+
+# Image extension for 2D images
+IMAGE_EXTENSION = "jpg"
+
